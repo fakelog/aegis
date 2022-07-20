24225da46371b1e9489d7ffeb8d803f4b0223641
Даня <48557356+fakelog@users.noreply.github.com>
2022-07-21 00:01:53 +0500

    server: now the chunks is not loaded on world startup

---
+++ src/main/java/com/github/fakelog/aegis/mixin/MinecraftServerMixin.java
@@ -0,0 +1,19 @@
+package com.github.fakelog.aegis.mixin;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerChunkManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin {
+
+    @Redirect(method = "prepareStartRegion", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/server/world/ServerChunkManager;getTotalChunksLoadedCount()I"))
+
+    public int onPrepareStartReg_redirectChunksLoaded(ServerChunkManager scm) {
+        return 441;
+    }
+}
