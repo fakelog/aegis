8e76dd1106acaae98db5e0a0d6b6e8642591e8c0
Даня <48557356+fakelog@users.noreply.github.com>
2022-07-21 19:50:49 +0500

    Aegis: добавлен Logger

--- src/main/java/com/github/fakelog/aegis/Aegis.java
+++ src/main/java/com/github/fakelog/aegis/Aegis.java
@@ -9,9 +9,13 @@ package com.github.fakelog.aegis;
 import com.github.fakelog.aegis.items.AegisItems;
 import net.fabricmc.api.ModInitializer;
 
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class Aegis implements ModInitializer {
 
 	public static final String MOD_ID = "aegis";
+	public static final Logger LOGGER = LogManager.getLogManager().getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
