e97dd7d9b61a44d91785958ec2b5a60b6e29ef53
Даня <48557356+fakelog@users.noreply.github.com>
2022-07-24 01:41:23 +0500

    Aegis: добавли вывод логов

--- src/main/java/com/github/fakelog/aegis/Aegis.java
+++ src/main/java/com/github/fakelog/aegis/Aegis.java
@@ -6,19 +6,24 @@
 
 package com.github.fakelog.aegis;
 
+import com.github.fakelog.aegis.block.AegisBlocks;
+import com.github.fakelog.aegis.item.AegisItems;
 import net.fabricmc.api.ModInitializer;
 import software.bernie.geckolib3.GeckoLib;
 
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Aegis implements ModInitializer {
 
 	public static final String MOD_ID = "aegis";
-	public static final Logger LOGGER = LogManager.getLogManager().getLogger(MOD_ID);
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+
+		AegisBlocks.registerAegisBlock();
+		AegisItems.registerAegisItems();
 
 		GeckoLib.initialize();
 	}
