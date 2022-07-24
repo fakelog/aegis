193894a3b778a831c3ea127fe974ab5e9c386e20
Даня <48557356+fakelog@users.noreply.github.com>
2022-07-24 11:14:47 +0500

    Aqua Ore: уменьшил кол-во генерируемой руды

--- src/main/java/com/github/fakelog/aegis/world/feature/AegisConfiguredFeatures.java
+++ src/main/java/com/github/fakelog/aegis/world/feature/AegisConfiguredFeatures.java
@@ -23,11 +23,11 @@ public class AegisConfiguredFeatures {
 
     public static final RegistryEntry<ConfiguredFeature<OreFeatureConfig, ?>> AQUA_ORE =
             ConfiguredFeatures.register("aqua_ore", Feature.ORE,
-                    new OreFeatureConfig(OVERWORLD_AQUA_ORES, 6));
+                    new OreFeatureConfig(OVERWORLD_AQUA_ORES, 2));
 
     public static final RegistryEntry<ConfiguredFeature<OreFeatureConfig, ?>> END_AQUA_ORE =
             ConfiguredFeatures.register("end_aqua_ore",Feature.ORE,
-                    new OreFeatureConfig(END_AQUA_ORES, 12));
+                    new OreFeatureConfig(END_AQUA_ORES, 4));
 
     public static void registerConfiguredFeatures() {
         Aegis.LOGGER.info("Registering ModConfiguredFeatures for " + Aegis.MOD_ID);
