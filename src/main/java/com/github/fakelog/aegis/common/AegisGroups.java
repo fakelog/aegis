5a4a0a00d214dcdda0a5c1ae2ac036f7a536ef15
Даня <48557356+fakelog@users.noreply.github.com>
2022-07-30 14:16:56 +0500

    Aqua Groups: теперь у вкладки мода иконка имеет вид яблОка, а также добавил группу на будущее

--- src/main/java/com/github/fakelog/aegis/common/AegisGroups.java
+++ src/main/java/com/github/fakelog/aegis/common/AegisGroups.java
@@ -7,7 +7,7 @@
 package com.github.fakelog.aegis.common;
 
 import com.github.fakelog.aegis.Aegis;
-import com.github.fakelog.aegis.common.item.AegisItems;
+import com.github.fakelog.aegis.common.block.AegisBlocks;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -16,16 +16,20 @@ import net.minecraft.util.Identifier;
 
 public class AegisGroups {
 
-    private static final String MOD_GROUP;
+    private static final String MOD_ITEMS_GROUP;
+    private static final String MOD_BLOCKS_GROUP;
+
     private static final ItemGroup AEGIS_ITEM_GROUP;
 
     public static final Item.Settings AEGIS_GROUP;
     public static final Item.Settings AEGIS_GROUP_FIREPROOF;
 
     static {
-        MOD_GROUP = "aegis_group";
-        AEGIS_ITEM_GROUP = FabricItemGroupBuilder.create(new Identifier(Aegis.MOD_ID, MOD_GROUP))
-                .icon(() -> new ItemStack(AegisItems.AQUA_HELMET))
+        MOD_ITEMS_GROUP = "aegis_group";
+        MOD_BLOCKS_GROUP = "aegis_blocks_group";
+
+        AEGIS_ITEM_GROUP = FabricItemGroupBuilder.create(new Identifier(Aegis.MOD_ID, MOD_ITEMS_GROUP))
+                .icon(() -> new ItemStack(AegisBlocks.APPLE_BLOCK))
                 .build();
 
         AEGIS_GROUP = new Item.Settings().group(AegisGroups.AEGIS_ITEM_GROUP).maxCount(64);
